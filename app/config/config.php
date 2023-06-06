eb9a0476fd0235f575c2d2270686e4c47c3ce938
ramos013 <114986495+ramos013@users.noreply.github.com>
2023-06-06 13:45:12 +0200

    last commit

--- app/config/config.php
+++ app/config/config.php
@@ -4,4 +4,4 @@ define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_NAME', 'mvc2209aP4toets');
-define('URLROOT', 'http://mvc-2209a-p4-toets.org/');
+define('URLROOT', 'http://mvc-2209a-toetsp4.org/');
